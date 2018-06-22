24d513383002f6f9bf0588086d578a53773e37fc
William Arustamyan <williamarustamyan@gmail.com>
2018-06-22 10:29:04 +0400

    Init empty application

---
+++ src/main/java/com/cjazz/CJazzApplication.java
@@ -0,0 +1,12 @@
+package com.cjazz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CJazzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CJazzApplication.class, args);
+	}
+}
